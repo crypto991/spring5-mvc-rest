922d6e541c6803675dbedfe65c6a1548f92f769b
Crypto991 <djordje2110@gmail.com>
2019-02-21 16:01:06 +0100

    Init Project

---
+++ src/main/java/crypto/org/api/v1/model/CategoryDTO.java
@@ -0,0 +1,7 @@
+package crypto.org.api.v1.model;
+
+public class CategoryDTO {
+
+    private Long id;
+    private String name;
+}
---
+++ src/main/java/crypto/org/repositories/CategoryRepository.java
@@ -0,0 +1,7 @@
+package crypto.org.repositories;
+
+import crypto.org.domain.Category;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CategoryRepository extends JpaRepository<Category, Long> {
+}
